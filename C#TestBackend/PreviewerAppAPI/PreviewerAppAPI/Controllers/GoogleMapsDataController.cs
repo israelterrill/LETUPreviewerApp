c68254c4920375c0b2484eedb7e665ad070b5b37
Israel Terrill <israelterrill@gmail.com>
2016-03-28 16:05:09 -0500

    Create C# backend test server.

---
+++ C#TestBackend/PreviewerAppAPI/PreviewerAppAPI/Controllers/GoogleMapsDataController.cs
@@ -0,0 +1,24 @@
+﻿using PreviewerAppAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace PreviewerAppAPI.Controllers
+{
+    public class GoogleMapsDataController : ApiController
+    {
+        public GoogleMapsData[] googleMapsLocations =
+        {
+            new GoogleMapsData { Name = "Heath Hardwick Hall", Code = "HHH", Lat = "32.467455", Long = "-94.726533" }
+        };
+
+        public HttpResponseMessage Get()
+        {
+            var request = Request.CreateResponse(HttpStatusCode.OK, googleMapsLocations);
+            request.Headers.Add("Access-Control-Allow-Origin", "*");
+            return request;
+        }
+    }
+}
---
+++ C#TestBackend/PreviewerAppAPI/PreviewerAppAPI/Models/QuestionData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PreviewerAppAPI.Models
+{
+    public class QuestionData
+    {
+        public String Query { get; set; }
+        public String Answer { get; set; }
+    }
+}
