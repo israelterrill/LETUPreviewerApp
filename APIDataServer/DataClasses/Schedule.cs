f166d3e906754b3230028e6069be288d2eadc970
israelterrill <israelterrill@gmail.com>
2016-04-09 16:52:09 -0500

    Update Schedule.cs
    modified regex pattern

--- APIDataServer/DataClasses/Schedule.cs
+++ APIDataServer/DataClasses/Schedule.cs
@@ -8,14 +8,14 @@ namespace DataClasses
 {
     public class Schedule
     {
-        public const string FILE_PATTERN = "(?<title>[^_]+)_(?<dates>.*)";
+        public const string FILE_PATTERN = @"(?<title>[^_]+)_(?<dates>.*)\.csv";
         public string ScheduleTitle { get; set; }
         public string ScheduleDates { get; set; }
         public BindingList<Event> Events { get; set; }
 
         public static Schedule FromCsv(string targetPath)
         {
-            var fileName = Path.GetFileNameWithoutExtension(targetPath);
+            var fileName = Path.GetFileName(targetPath);
             var rgxFileName = new Regex(FILE_PATTERN);
             if (!rgxFileName.IsMatch(fileName)) throw new FormatException(string.Format("Target file '{0}' does not conform to file name standards.", targetPath));
             var match = rgxFileName.Match(fileName);
@@ -32,7 +32,7 @@ namespace DataClasses
         {
             var rgxScheduleFile = new Regex(FILE_PATTERN);
             return (from file in Directory.GetFiles(targetDir)
-                    where rgxScheduleFile.IsMatch(Path.GetFileNameWithoutExtension(file))
+                    where rgxScheduleFile.IsMatch(Path.GetFileName(file))
                     select FromCsv(file)).ToArray();
         }
 
